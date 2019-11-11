260859eb8488a8ef9519a27e4f4612c57c4473c3
Travis CI User <travis@example.org>
2019-11-11 02:38:58 +0000

    Updates

---
+++ component---src-pages-about-js-9cd4379616097c87fef7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return c}));var a=n("q1tI"),r=n.n(a),l=n("Bl7J"),u=n("vrFN"),c="3915270372";t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("article",null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement(u.a,{title:"About"}),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}})))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-9cd4379616097c87fef7.js.map
---
+++ styles-db4ba60dbd366867b266.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"6MIl":function(n,o,p){},XjQp:function(n,o,p){},ae5o:function(n,o,p){}}]);
+//# sourceMappingURL=styles-db4ba60dbd366867b266.js.map
