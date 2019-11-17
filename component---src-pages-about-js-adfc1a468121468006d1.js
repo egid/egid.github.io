149a835f08d2071f7e2c7e8d46c6916b473eccff
Travis CI User <travis@example.org>
2019-11-17 18:21:11 +0000

    Updates

---
+++ component---src-pages-about-js-adfc1a468121468006d1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return c}));var a=n("q1tI"),r=n.n(a),l=n("Bl7J"),u=n("vrFN"),c="584567866";t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("article",null,r.a.createElement("h1",null,t.frontmatter.title),r.a.createElement(u.a,{title:"About"}),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}})))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-adfc1a468121468006d1.js.map
